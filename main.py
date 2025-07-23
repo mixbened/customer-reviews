1482b727410cc4395eef8118d50183a8c26e886a
mixbened <mixbened@gmail.com>
2025-07-23 14:03:14 +0200

    feat: fastapi setup

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from typing import Union
+from pydantic import BaseModel
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/item")
+def read_item(item: Item):
+    return {"item_sent": item, }
